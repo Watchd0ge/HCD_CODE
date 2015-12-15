8049a2401b72ecb61a88ee59f1bc65e2cf9c0cff
JJ Lau <j.j.j.lau@live.com>
2015-12-16 01:10:48 +0900

    Added logging in functionality

--- app/src/main/java/org/hcd/hcdproject/MainActivity_login.java
+++ app/src/main/java/org/hcd/hcdproject/MainActivity_login.java
@@ -1,22 +1,83 @@
 package org.hcd.hcdproject;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity_login extends AppCompatActivity {
+
+    private Firebase myFirebaseRef;
+    private SQLiteDatabase localDB;
+    private Button login_btn;
+    private EditText voucher_id_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
 
+        // FIREBASE SETUP
+        Firebase.setAndroidContext(this);
+        myFirebaseRef = new Firebase("https://hcd-firebase.firebaseio.com/");
+        initViews();
+
     }
 
     public void onButtonEnterClicked(View v){
         Intent intent = new Intent(getApplicationContext(), MainActivity_login1.class);
         startActivity(intent);
+    }
+
+    private void initViews(){
+        login_btn = (Button) findViewById(R.id.login_btn);
+        voucher_id_txt = (EditText) findViewById(R.id.voucher_id);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (voucher_id_txt.getText().toString().matches("")) {
+                    showError();
+                } else {
+                    myFirebaseRef.child("Profiles").child(voucher_id_txt.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Log.wtf("IT WORKED", "[" + voucher_id_txt.getText().toString() + "]");
+                            if (dataSnapshot.exists()){
+                                Log.wtf("THERE IS SOMETHING", "WHOOP");
+                                Intent intent = new Intent(MainActivity_login.this, MainActivity_login1.class);
+                                startActivity(intent);
+                            } else {
+                                showError();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(FirebaseError firebaseError) {
+                            showError();
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+    private void showError() {
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        voucher_id_txt.setText("");
+        voucher_id_txt.startAnimation(shake);
+        voucher_id_txt.setError("Your voucher ID is incorrect."); // KOREAN
     }
 
 }
