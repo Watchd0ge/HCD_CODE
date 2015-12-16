3fe65be2ccb14387d1ede83cdf266fe4843d8263
Taylor <rhj9892@gmail.com>
2015-12-16 09:24:14 +0900

    CHECKPOINT 3

--- app/src/main/java/org/hcd/hcdproject/MainActivity_choosehelper.java
+++ app/src/main/java/org/hcd/hcdproject/MainActivity_choosehelper.java
@@ -1,15 +1,50 @@
 package org.hcd.hcdproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity_choosehelper extends AppCompatActivity {
-
+//    private CheckBox need, noneed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_choosehelper);
+        initView();
+    }
+    public void onButtonNextClicked(View v){
+        startActivity(new Intent(getApplicationContext(), MainActivity_choosehelper1.class));
     }
 
+
+//    public void onMorningClicked(View v) {
+//        RadioButton morning = (RadioButton) findViewById(R.id.morning);
+////        morning.setChecked(!morning.toggle(););
+//    }
+//    public void onAfternoonClicked(View v) {
+//        RadioButton afternoon = (RadioButton) findViewById(R.id.afternoon);
+//        afternoon.setChecked( !afternoon.isChecked() );
+//    }
+//
+    private void initView(){
+        final CheckBox need = (CheckBox) findViewById(R.id.needcar);
+        final CheckBox noneed = (CheckBox) findViewById(R.id.noneedcar);
+        need.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(noneed.isChecked())
+                    noneed.setChecked(false);
+            }
+        });
+        noneed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(need.isChecked())
+                    need.setChecked(false);
+            }
+        });
+    }
 
 }
--- app/src/main/java/org/hcd/hcdproject/MainActivity_match2.java
+++ app/src/main/java/org/hcd/hcdproject/MainActivity_match2.java
@@ -1,12 +1,10 @@
 package org.hcd.hcdproject;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity_match2 extends AppCompatActivity {
@@ -30,11 +28,6 @@ public class MainActivity_match2 extends AppCompatActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int) (width * .8), (int) (height * .8));
-    }
-
-    public void onButtonCloseClicked (View v){
-        Intent intent = new Intent(getApplicationContext(), MainActivity_match.class);
-        startActivity(intent);
     }
 
 }
