b8d4cf6b28e26dcfa2a8767f8f7a012f54e798e3
JJ Lau <j.j.j.lau@live.com>
2015-12-16 13:55:54 +0900

    SIGNUP2

--- app/src/main/java/org/hcd/hcdproject/MainActivity_signup1.java
+++ app/src/main/java/org/hcd/hcdproject/MainActivity_signup1.java
@@ -1,7 +1,9 @@
 package org.hcd.hcdproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -32,6 +34,7 @@ public class MainActivity_signup1 extends AppCompatActivity {
     private TextView region;
     private TextView age;
     private Button moreInfo;
+    private String voucher_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,7 @@ public class MainActivity_signup1 extends AppCompatActivity {
         setContentView(R.layout.activity_main_signup1);
         Firebase.setAndroidContext(this);
         myFirebaseRef = new Firebase("https://hcd-firebase.firebaseio.com/");
+        voucher_number = getIntent().getStringExtra("VOUCHER_ID");
         initView();
     }
 
@@ -47,12 +51,43 @@ public class MainActivity_signup1 extends AppCompatActivity {
         intent.putExtra("VOUCHER_ID", et.getText().toString());
         startActivity(intent);
     }
-    
+
 
     public void onButtonEnterClicked(View v) {
         Intent intent = new Intent(getApplicationContext(), MainActivity_signup1.class);
-        intent.putExtra("VOUCHER_IDr", et.getText().toString());
+        intent.putExtra("VOUCHER_ID", et.getText().toString());
         startActivity(intent);
+    }
+
+    public void onButtonRegisterClicked(View v) {
+        runDialog();
+    }
+
+    private void runDialog(){
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("등록 완료!")
+                .setMessage("자신에게 맞는 활동보조사를 찾아보세요!")
+                .setNegativeButton("홈 화면으로", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity_signup1.this, MainActivity_login1.class);
+                        intent.putExtra("VOUCHER_ID", voucher_number);
+                        startActivity(intent);
+                    }
+
+                })
+                .setPositiveButton("활동보조사 선택", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity_signup1.this, MainActivity_choosehelper.class);
+                        intent.putExtra("VOUCHER_ID", voucher_number);
+                        startActivity(intent);
+                    }
+
+                })
+                .show();
     }
 
     private void initView(){
@@ -76,6 +111,7 @@ public class MainActivity_signup1 extends AppCompatActivity {
         });
 
         et.setText(voucher_number);
+        et.setFocusable(false);
 
         myFirebaseRef.child("Profiles").child(voucher_number).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
